868dc9c53806ecffa33545768942899eb8637541
A1v0-do <alvomutisya@gmail.com>
2026-09-17 18:37:35 +0300

    Day 1: added hello.py and gitignore setup

---
+++ hello.py
@@ -0,0 +1,30 @@
+# strings
+name="Vic"
+food="kuku"
+
+# integers
+age= 28
+chapatis=10
+
+# floats
+gpa=3.9
+
+# boolean
+is_student= False
+you_genius=True
+
+print(f"My name is {name}")
+print(f"I love {food}")
+print(f"I am {age} years old")
+print(f"I eat {chapatis} chapatis")
+print(f"I have a cumulative of {gpa}")
+
+if is_student:
+    print("I am a student")
+else:
+    print("I am not a student")
+
+    if you_genius:
+        print("I believe I am a genius")
+    else:
+        print("I am stupid")
